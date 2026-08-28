0241b16b02e330d54bf2687298f87e149b1dd8db
shuhangli <shuhangli@txcombo.com>
2026-08-28 16:38:30 +0800

    Refactor EsRestLanguage to extend SqlLanguageDialectBase, adding support for Elasticsearch as a SQL dialect. Implement methods for token handling, system variables, and statement separators. Update plugin.xml to register the new SQL dialect and evaluation helper for ElasticsearchREST.

--- plugin/src/main/java/io/github/suhli/datagrip/elasticsearch/plugin/language/EsRestLanguage.java
+++ plugin/src/main/java/io/github/suhli/datagrip/elasticsearch/plugin/language/EsRestLanguage.java
@@ -1,11 +1,42 @@
 package io.github.suhli.datagrip.elasticsearch.plugin.language;
 
-import com.intellij.lang.Language;
+import com.intellij.database.Dbms;
+import com.intellij.sql.dialects.base.SqlLanguageDialectBase;
+import com.intellij.sql.dialects.base.TokensHelper;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.tree.TokenSet;
 
-public final class EsRestLanguage extends Language {
+import java.util.Set;
+
+public final class EsRestLanguage extends SqlLanguageDialectBase {
     public static final EsRestLanguage INSTANCE = new EsRestLanguage();
 
     private EsRestLanguage() {
         super("ElasticsearchREST");
+    }
+
+    @Override
+    protected TokensHelper createTokensHelper() {
+        return createTokensHelper(EsRestTokenTypes.class);
+    }
+
+    @Override
+    public Dbms getDbms() {
+        return Dbms.byName("ELASTICSEARCH");
+    }
+
+    @Override
+    public boolean isOperatorSupported(IElementType type) {
+        return false;
+    }
+
+    @Override
+    public Set<String> getSystemVariables() {
+        return Set.of();
+    }
+
+    @Override
+    public TokenSet getStatementSeparators() {
+        return TokenSet.EMPTY;
     }
 }
